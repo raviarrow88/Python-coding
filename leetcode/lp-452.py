db62099e634fc171f90af9cd6c773c6e3bfc87a2
ravi <raviarrow88@gmail.com>
2020-08-22 22:26:39 +0530

    lp-452.py

---
+++ leetcode/lp-452.py
@@ -0,0 +1,41 @@
+# 452. Minimum Number of Arrows to Burst Balloons
+# There are a number of spherical balloons spread in two-dimensional space. For each balloon, provided input is the start and end coordinates of the horizontal diameter. Since it's horizontal, y-coordinates don't matter and hence the x-coordinates of start and end of the diameter suffice. Start is always smaller than end. There will be at most 104 balloons.
+#
+# An arrow can be shot up exactly vertically from different points along the x-axis. A balloon with xstart and xend bursts by an arrow shot at x if xstart ≤ x ≤ xend. There is no limit to the number of arrows that can be shot. An arrow once shot keeps travelling up infinitely. The problem is to find the minimum number of arrows that must be shot to burst all balloons.
+#
+# Example:
+#
+# Input:
+# [[10,16], [2,8], [1,6], [7,12]]
+#
+# Output:
+# 2
+#
+# Explanation:
+# One way is to shoot one arrow for example at x = 6 (bursting the balloons [2,8] and [1,6]) and another arrow at x = 11 (bursting the other two balloons).
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+
+        k = sorted(points,key=lambda x:x[1])
+
+
+        s =[]
+        e=[]
+        for i in range(len(k)):
+            s.append(k[i][0])
+            e.append(k[i][1])
+
+        arr_count =1
+
+
+        end = e[0]
+        for j in range(len(points)):
+            if (s[j] > end):
+                end = e[j]
+                arr_count+=1
+
+
+        return arr_count
