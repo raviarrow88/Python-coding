64539f23900c80c4adceb397abf033ed26df5dbc
ravi <raviarrow88@gmail.com>
2020-12-08 22:59:50 +0530

    lp-139.py

---
+++ leetcode/lp-139.py
@@ -0,0 +1,25 @@
+# 139. Word Break
+# Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+#
+# Note:
+#
+# The same word in the dictionary may be reused multiple times in the segmentation.
+# You may assume the dictionary does not contain duplicate words.
+# Example 1:
+#
+# Input: s = "leetcode", wordDict = ["leet", "code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False for i in range(len(s)+1)]
+        dp[0]=True
+
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i]=True
+                    break
+
+        return dp[-1]
